c9eb288effbb1a327b55e52745ddcc00547d0975
Ian Blenke <ian@blenke.com>
2018-03-08 07:42:18 -0500

    Simplifying run.sh script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_FALLBACK_THREADS=${NUM_FALLBACK_THREADS:-$(( $(nproc) * 2 ))}
 
-exec ccminer -a x16r --num-fallback-threads $NUM_FALLBACK_THREADS -o $URL -u $USER -p $PASS $@
+exec ccminer -a x16r -o $URL -u $USER -p $PASS $@
