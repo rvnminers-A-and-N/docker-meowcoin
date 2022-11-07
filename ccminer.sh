f1a898705318b37a90e5ff853eb02ff8c1ab028d
rvnminers-A-and-N <86896075+rvnminers-A-and-N@users.noreply.github.com>
2022-11-07 15:52:40 -0500

    MEWC

--- ccminer.sh
+++ ccminer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_FALLBACK_THREADS=${NUM_FALLBACK_THREADS:-$(( $(nproc) * 2 ))}
 
-exec ccminer -a x16r -o $URL -u $USERNAME -p $PASSWORD $@
+exec ccminer -a kawpow -o $URL -u $USERNAME -p $PASSWORD $@
